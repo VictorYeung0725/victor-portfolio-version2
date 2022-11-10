c2e62e653da48156d703906e59debbad5dd6dd22
VictorYeung0725 <96713542+VictorYeung0725@users.noreply.github.com>
2022-11-10 21:38:00 +0800

    fix the RWD of text animation

--- app.js
+++ app.js
@@ -13,3 +13,16 @@ setInterval(function () {
 
   showNext.setAttribute('data-show', '');
 }, 2000);
+
+//typing effect start
+// const h1 = document.querySelector('h1');
+// h1.innerHTML = h1.textContent.replace(/\S/g, '<span>$&</span>');
+
+// let delay = 0;
+// document.querySelectorAll('span').forEach((span, index) => {
+//   delay += 0.1;
+
+//   if (index === 6) delay += 0.3;
+
+//   span.style.setProperty('--delay', `${delay}s`);
+// });
