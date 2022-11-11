244708e50d56c229056dd6f7ccd097b32abd97b3
VictorYeung0725 <96713542+VictorYeung0725@users.noreply.github.com>
2022-11-11 09:22:09 +0800

    adding loading page

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
-setInterval(function () {
-  const show = document.querySelector('span[data-show]');
-  const showNext =
-    show.nextElementSibling || document.querySelector('span:first-child');
+// load content after finished loading
+const container = documen.querySelector('#container');
 
-  const up = document.querySelector('span[data-up]');
+container.addEventListner('DOMContentLoaded', function () {
+  container.style.display = 'block';
 
-  if (up) {
-    up.removeAttribute('data-up');
-  }
-  show.removeAttribute('data-show');
-  show.setAttribute('data-up', '');
+  setInterval(function () {
+    const show = document.querySelector('span[data-show]');
+    const showNext =
+      show.nextElementSibling || document.querySelector('span:first-child');
 
-  showNext.setAttribute('data-show', '');
-}, 2000);
+    const up = document.querySelector('span[data-up]');
+
+    if (up) {
+      up.removeAttribute('data-up');
+    }
+    show.removeAttribute('data-show');
+    show.setAttribute('data-up', '');
+
+    showNext.setAttribute('data-show', '');
+  }, 2000);
+});
 
 //typing effect start
 // const h1 = document.querySelector('h1');
